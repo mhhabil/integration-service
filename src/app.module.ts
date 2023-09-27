e6b9db492b65b1aa06a3a98be659a1b22f4a22de
habil-GIC <habil@gic-indonesia.com>
2023-09-27 17:04:52 +0700

    [Auth] add RBAC class JwtAuthGuard for authorization

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,10 @@ import { TerminusModule } from '@nestjs/terminus';
 import { ThrottlerModule } from '@nestjs/throttler';
 import { RedisModule } from '@liaoliaots/nestjs-redis';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { InformationController } from './modules/information/controllers/information.controller';
+import { InformationService } from './modules/information/services/information.service';
+import RequiredModules from './modules';
 
 @Module({
   imports: [
@@ -19,16 +23,28 @@ import { ConfigModule } from '@nestjs/config';
     RedisModule.forRoot({
       readyLog: true,
       errorLog: true,
-      config: {
-        host: process.env.REDIS_HOST,
-        port: +process.env.REDIS_PORT,
-        username: process.env.REDIS_USERNAME,
-        password: process.env.REDIS_PASSWORD,
-        db: +process.env.REDIS_DATABASE,
-      },
+      config: [
+        {
+          host: process.env.REDIS_HOST,
+          port: +process.env.REDIS_PORT,
+          username: process.env.REDIS_USERNAME,
+          password: process.env.REDIS_PASSWORD,
+          db: +process.env.REDIS_DATABASE,
+        },
+        {
+          namespace: 'RBAC',
+          host: process.env.RBAC_REDIS_HOST,
+          port: +process.env.RBAC_REDIS_PORT,
+          username: process.env.RBAC_REDIS_USERNAME,
+          password: process.env.RBAC_REDIS_PASSWORD,
+          db: +process.env.RBAC_REDIS_DATABASE,
+        },
+      ],
     }),
+    AuthModule,
+    ...RequiredModules,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, InformationController],
+  providers: [AppService, InformationService],
 })
 export class AppModule {}
