0b2960e7ea956b61070a2fadc6dcef3f40ecfe28
Muhammad Habil <habil@gic-indonesia.com>
2023-10-02 17:25:39 +0700

    [Information] add JWT Authguard and retrieve user data with token

--- src/modules/information/services/information.service.ts
+++ src/modules/information/services/information.service.ts
@@ -1,20 +1,44 @@
 import { Injectable } from '@nestjs/common';
 import { Information } from '../interfaces/information';
 import { RedisSharedService } from 'src/shared/services/redis.service';
+import { DatetimeService } from 'src/shared/services/datetime.service';
 
 @Injectable()
 export class InformationService {
-  constructor(private redisService: RedisSharedService) {}
-  create(information: Information) {
-    this.redisService.set(
+  constructor(
+    private redisService: RedisSharedService,
+    private datetimeService: DatetimeService,
+  ) {}
+
+  async create(information: Information, userId: string) {
+    const currentDatetime = this.datetimeService.getCurrentDatetime();
+    const result = await this.redisService.get(
       `Informasi:{${information.hospital_id}}`,
-      '$',
-      information,
+      '.',
     );
+    if (result && result !== null) {
+      const params = {
+        ...information,
+        created_at: result.created_at,
+        updated_at: currentDatetime,
+        created_by: result.created_by,
+        updated_by: userId,
+      };
+      this.redisService.set(`Informasi:{${params.hospital_id}}`, '$', params);
+    } else {
+      const params = {
+        ...information,
+        created_at: currentDatetime,
+        updated_at: currentDatetime,
+        created_by: userId,
+        updated_by: userId,
+      };
+      this.redisService.set(`Informasi:{${params.hospital_id}}`, '$', params);
+    }
   }
 
-  async findAll(id: string): Promise<Information> {
-    const test: any = await this.redisService.get(`Informasi:{${id}}`, '$');
-    return test;
+  async findById(id: string): Promise<Information> {
+    const result = await this.redisService.get(`Informasi:{${id}}`, '.');
+    return result;
   }
 }
